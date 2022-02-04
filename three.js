d6b4116d213bf6e0d519cd4f28f2d077b6afa40c
sanjoy <sanjoykundu187@gamil.com>
2022-02-04 00:22:24 -0500

    first commit

---
+++ three.js
@@ -0,0 +1,17 @@
+/*==========================================================
+৩. দুইটা ভেরিয়েবল এর মধ্যে যোগ, বিয়োগ, গুণ, ভাগ কিভাবে করতে হয় সেটা কি জানো। অর্থাৎ তুমি কি +, -, *, /, %এইগুলার ব্যবহার জানো। তাহলে নাম্বার টাইপের দুইটা ভেরিয়েবল লিখো তারপর তাদের যোগ করে সেটার মান আরেকটা ভেরিয়েবল এ রাখো। একইভাবে ওই দুইটা ভেরিয়েবল এর মধ্যে বিয়োগ, গুন, ভাগ এবং ভাগশেষ বের করো। 
+=================================================================*/
+
+
+var firstNumber = 100;
+var secondNumber = 7;
+var addition = firstNumber + secondNumber;
+console.log("Total addition is ", addition);
+var subtruction = firstNumber - secondNumber;
+console.log("Total subtruction is ", subtruction);
+var multiplication = firstNumber * secondNumber;
+console.log("Total multiplication is ", multiplication);
+var division = firstNumber / secondNumber;
+console.log("Total division ", division);
+var modulus = firstNumber % secondNumber;
+console.log(modulus);
